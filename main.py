bd9362c5901ac70ccfd9c6448191bb1b4a2dfb5f
Gaurav Singh <grv08singh@gmail.com>
2026-06-08 00:22:41 +0530

    model trainer code completed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 from textSummarizer.pipelines.s01_data_ingestion_pipeline import DataIngestionPipeline
 from textSummarizer.pipelines.s02_data_validation_pipeline import DataValidationPipeline
 from textSummarizer.pipelines.s03_data_transformation_pipeline import DataTransformationPipeline
+from textSummarizer.pipelines.s04_model_trainer_pipeline import ModelTrainerPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -31,6 +32,16 @@ try:
     logger.info(f">>>>>>>>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<<<<<<<")
     data_validation = DataTransformationPipeline()
     data_validation.main()
+    logger.info(f">>>>>>>>>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training Stage"
+try:
+    logger.info(f">>>>>>>>>>>>>>>> {STAGE_NAME} started <<<<<<<<<<<<<<<<")
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
     logger.info(f">>>>>>>>>>>>>>>> {STAGE_NAME} completed <<<<<<<<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
